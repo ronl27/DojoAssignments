a227d9c9f2ac028b2cbcdb7a41c8bd7b15ebd091
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-04-04 15:12:39 -0700

    Updated methods.rb to show Class methods

--- Ruby/OOP/Methods/methods.rb
+++ Ruby/OOP/Methods/methods.rb
@@ -1,22 +1,57 @@
 ### INSTANCE METHOD ###
 # instance methods are just regular methods that are defined inside the Class Definition
 
+# class User
+#   # creating instance methos that get and set the first_name and last_name attributes
+#   attr_accessor :first_name, :last_name
+#
+#   def initialize(f_name, l_name) #method 1
+#     @first_name = f_name
+#     @last_name = l_name
+#   end
+#
+#   def full_name #method2
+#     puts "I am #{@first_name} #{@last_name}"
+#   end
+#
+#   def say_hello #method3
+#     puts "Aloha"
+#   end
+# end
+# r = User.new("Ryan", "Bautista")
+# r.full_name
+# r.say_hello
+
+
+### CLASS METHODS ####
+#class methods are prefixedd by SELF followed by a period. these methods are executed only at the CLASS level(which is User) and are not available to the instances
+
 class User
-  # creating instance methos that get and set the first_name and last_name attributes
   attr_accessor :first_name, :last_name
-  def initialize(f_name, l_name)
+
+  def initialize(f_name, l_name) #method 1
     @first_name = f_name
     @last_name = l_name
   end
 
-  def full_name
+  def full_name #method2
     puts "I am #{@first_name} #{@last_name}"
   end
 
-  def say_hello
+  def say_hello #method3
     puts "Aloha"
   end
+
+  def self.foo
+    puts "You have reached the Class method"
+  end
 end
-r = User.new("Ryan", "Bautista")
-r.full_name
-r.say_hello
+u = User.new("Ryan", "Bautista")
+u.full_name
+u.say_hello
+
+# using
+# u.foo # => NoMethodError: undefined method 'foo'
+
+# must use the actual class name to reference the self.foo
+User.foo # => "You have reached the Class method"
