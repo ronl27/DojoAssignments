12d788be08a63ef90ab3a004389ce17bcf278b2f
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-04-13 19:58:43 -0700

    Adding changed files

--- Ruby/OOP/Methods/methods.rb
+++ Ruby/OOP/Methods/methods.rb
@@ -1,26 +1,26 @@
 ### INSTANCE METHOD ###
 # instance methods are just regular methods that are defined inside the Class Definition
 
-# class User
-#   # creating instance methos that get and set the first_name and last_name attributes
-#   attr_accessor :first_name, :last_name
-#
-#   def initialize(f_name, l_name) #method 1
-#     @first_name = f_name
-#     @last_name = l_name
-#   end
-#
-#   def full_name #method2
-#     puts "I am #{@first_name} #{@last_name}"
-#   end
-#
-#   def say_hello #method3
-#     puts "Aloha"
-#   end
-# end
-# r = User.new("Ryan", "Bautista")
-# r.full_name
-# r.say_hello
+class User
+  # creating instance methos that get and set the first_name and last_name attributes
+  attr_accessor :first_name, :last_name
+
+  def initialize(f_name, l_name) #method 1
+    @first_name = f_name
+    @last_name = l_name
+  end
+
+  def full_name #method2
+    puts "I am #{@first_name} #{@last_name}"
+  end
+
+  def say_hello #method3
+    puts "Aloha"
+  end
+end
+r = User.new("Ryan", "Bautista")
+r.full_name
+r.say_hello
 
 
 ### CLASS METHODS ####
