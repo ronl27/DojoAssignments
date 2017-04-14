e62eb967047ac352c3ff01785641bf70095878b5
Ronnel Bautista <ronnel.j.bautista@gmail.com>
2017-04-13 20:03:07 -0700

    Routes Practice added

---
+++ Ruby/rails/first_vagrant_box/routes_practice/command.rb
@@ -0,0 +1,32 @@
+#Routes Practice
+
+# When a http GET request to 'http://localhost:3000/hello' is sent, have it display a simple text that says 'Hello CodingDojo!'
+#
+# When a http GET request to 'http://localhost:3000/say/hello' is sent, have it display a simple text that says 'Saying Hello!'
+#
+# When a http GET request to 'http;//localhost:3000/say/hello/joe' is sent, have it display a simple text that says 'Saying Hello Joe!'
+#
+# When a http GET request to 'http;//localhost:3000/say/hello/michael' is sent, have it redirect to a url of 'say/hello/joe'
+#
+# When a http GET request to 'http://localhost:3000' is sent, have the 'say' controller's index method handle the response; have it render a text that says 'What do you want me to say???'
+#
+# When a http GET request to 'http://localhost:3000/times' is sent, have it tell you how many times you've visited that page.
+#
+# When you first visit that url, it should say 'You visited this url 1 time'. Keep reloading the page to make sure the number goes up.
+#
+# When a http GET request to 'http://localhost:3000/times/restart' is sent, have it reset the session and display a text that says 'Destroyed the session!'.
+
+
+
+##### Create Controller ######
+
+#in the console
+# 'rails g controller Displays'
+
+#check out the syntax in app/controllers/displays_controller
+
+
+
+####### In config/routes.rb #######
+
+# check out the syntax in config/routes.rb
